a218114f3cad9d4091e9d37e4144e72370e9f946
Lanang Jati <>
2019-01-15 09:47:36 +0700

    first-session

---
+++ src/main/java/com/lanjati/Flower.java
@@ -0,0 +1,63 @@
+package com.lanjati;
+
+/**
+ * @author Lanang Jati
+ */
+public class Flower {
+
+    private String color;
+    private Long petalAmount;
+    private boolean isHasThorn;
+    private static final String COLOR= "red,yellow,green";
+
+    public Flower() {
+        System.out.println("Empty constructor called");
+    }
+
+    public Flower(String color) {
+        this(color, 5l, true);
+        System.out.println("Constructor color");
+    }
+
+    public Flower(String color, Long petalAmount, boolean isHasThorn) {
+        this.color = color;
+        this.petalAmount = petalAmount;
+        this.isHasThorn = isHasThorn;
+        System.out.println("Constructor all");
+
+    }
+
+    public boolean isBeauty() {
+        if (color.equalsIgnoreCase("red") && petalAmount >= 5){
+            return true;
+        } else return isHasThorn;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (COLOR.contains(color.toLowerCase())) {
+            this.color = color;
+        } else {
+            this.color = "Undefined";
+        }
+    }
+
+    public Long getPetalAmount() {
+        return petalAmount;
+    }
+
+    public void setPetalAmount(Long petalAmount) {
+        this.petalAmount = petalAmount;
+    }
+
+    public boolean isHasThorn() {
+        return isHasThorn;
+    }
+
+    public void setHasThorn(boolean hasThorn) {
+        isHasThorn = hasThorn;
+    }
+}
--- src/main/java/com/lanjati/Main.java
+++ src/main/java/com/lanjati/Main.java
@@ -7,5 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("In main method");
+
+        Flower rose = new Flower("YELLOW");
+
+        System.out.println(rose.getColor());
+        System.out.println(rose.getPetalAmount());
+
+        if (rose.isBeauty()){
+            System.out.println("Flower is beauty");
+        }else{
+            System.out.println("Flower is not beauty");
+        }
+
     }
 }
